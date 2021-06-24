ebe0b4faa3f892b8317645bd72af7276e4552a77
bjcgit <bryan.bjc@outlook.com>
2021-06-23 20:40:41 -0500

    terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { SharedModule } from '../app/shared/shared.module';
+import { GifsModule } from './gifs/gifs.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    SharedModule,
+    GifsModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
+  
+  
 })
 export class AppModule { }
